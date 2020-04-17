b9bd3efb1bddc1362bc3b850a6786cbdf3cbb6c3
Lisa Campbell <lisa@Lisas-MBP.lan>
2020-04-17 08:15:17 -0700

    mvp with 992 moves.

--- adv.py
+++ adv.py
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -27,7 +27,70 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+
+
+# Creates an empty traversal path list
 traversal_path = []
+
+def get_possible_directions(current_room, visited_rooms):
+    # creates empty list of valid exits
+    valid_exits = []
+    # Creates list with all exits
+    for exit in current_room.get_exits():
+        # if the current room has not been visited
+        if room_graph[current_room.id][1][exit] not in visited_rooms:
+            # mark as visited and add the exit
+            valid_exits.append(exit)
+    #return the updated valid exits
+    return valid_exits
+
+# Fill this out with directions to walk
+def find_rooms():
+    # variables:
+
+    # creates empty set of visted rooms
+    visited_rooms = set()
+    # adds the player's current room to the set of visited rooms
+    visited_rooms.add(player.current_room.id)
+    # empty list to reverse the path out of the room
+    backward_path = []
+
+    # while loops the length of the visted rooms is less than 500
+    while len(visited_rooms) < len(room_graph.keys()):
+        #sets the player in the current room
+        current_room = player.current_room.id
+        # traverse through to get valid exits
+        valid_exits = get_possible_directions(player.current_room, visited_rooms)
+
+        # if there are no valid exits go backwards
+        if len(valid_exits) == 0: 
+            # removes the last path used
+            exit_direction = backward_path.pop()
+            player.travel(exit_direction)
+            # adds the exit_direction/path 
+            traversal_path.append(exit_direction) # this is a path
+            continue # continue the while loop
+
+        # if the room hasn't been visited
+        for exit_direction in valid_exits:
+            # add the current room to visited
+            visited_rooms.add(room_graph[current_room][1][exit_direction])
+            # adds the exit_direction/path 
+            traversal_path.append(exit_direction)  # this is a path
+
+            # the backwards path back out
+            if exit_direction == "n":
+                backward_path.append("s")
+            elif exit_direction == "s":
+                backward_path.append("n")
+            elif exit_direction == "w":
+                backward_path.append("e")
+            else:
+                backward_path.append("w")
+            player.travel(exit_direction)
+            break
+
+find_rooms()
 
 
 
